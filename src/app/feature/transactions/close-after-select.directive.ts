e62d34e82f048be56b26e3d1e3ec7f581c9dc325
Patrick Dobler <patrick.dobler@gmail.com>
2023-01-22 22:52:05 +0100

    bugfixes

--- src/app/feature/transactions/close-after-select.directive.ts
+++ src/app/feature/transactions/close-after-select.directive.ts
@@ -2,7 +2,7 @@ import {Directive, OnInit} from "@angular/core";
 import {MatSelect} from "@angular/material/select";
 
 @Directive({
-  selector: '[closeAfterSelect]'
+  selector: '[close-after-select]'
 })
 export class CloseAfterSelectDirective implements OnInit {
 
