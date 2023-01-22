56e4cccdb984329acbc57587c35a385a382ee894
Patrick Dobler <patrick.dobler@gmail.com>
2023-01-22 15:46:34 +0100

    working on build pipeline

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  sumup_clientSecret: '{SUMUP_CLIENTSECRET}',
+  sumup_clientId: '{SUMUP_CLIENTID}',
+  sumup_redirectUri: '{SUMUP_REDIRECTURI}',
+  sumup_logoutUrl: '{SUMUP_LOGOUTURL}',
+  pageLimit: 100,
+  title: "{APP_TITLE}"
+};
